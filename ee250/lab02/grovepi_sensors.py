58d53c2c521d1033b987667e6f43f115bbc2ef12
Daniel D'Souza <dmdsouza@usc.edu>
2020-09-08 21:18:56 -0700

    Changed the sleep timer

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -46,7 +46,7 @@ setRGB(0,128,64)
 # Slowly change the colors every 0.01 seconds.
 for c in range(0,255):
     setRGB(c,255-c,0)
-    time.sleep(1)
+    time.sleep(0.01)
 
 setRGB(0,255,0)
 setText("Bye bye, this should wrap")
