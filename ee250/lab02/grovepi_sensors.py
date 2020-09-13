5b7f00b2a16dfa4b0be4c5a5d36a91dece269617
Daniel D'Souza <dmdsouza@usc.edu>
2020-09-12 18:09:58 -0700

    Added code for ultrasonic sensor read and display on LCD

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -1,8 +1,7 @@
 """ EE 250L Lab 02: GrovePi Sensors
 
-List team members here.
+https://github.com/dmdsouza1/GrovePi-EE250
 
-Insert Github repository link here.
 """
 
 """python3 interpreters in Ubuntu (and other linux distros) will look in a 
@@ -14,7 +13,6 @@ Examples of some default directories are (but not limited to):
 The `sys` module, however, is a builtin that is written in and compiled in C for
 performance. Because of this, you will not find this in the default directories.
 """
-'''
 import sys
 import time
 # By appending the folder of all the GrovePi libraries to the system path here,
@@ -24,46 +22,38 @@ sys.path.append('../../Software/Python/')
 sys.path.append('../../Software/Python/grove_rgb_lcd')
 
 import grovepi
+#from grove_rgb_lcd import *
 
-"""This if-statement checks if you are running this python file directly. That 
-is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will 
-be true"""
+# This if-statement checks if you are running this python file directly. That 
+# is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will 
+# be true
 if __name__ == '__main__':
+    SLIDE = 0   #A0
     PORT = 4    # D4
+    previous_sensor_value = -1
+
+    time.sleep(1)
+    setRGB(0,255,0)
+    setText(" ")
 
     while True:
+
         #So we do not poll the sensors too quickly which may introduce noise,
         #sleep for a reasonable time of 200ms between each iteration.
         time.sleep(0.2)
+        try: 
+            sensor_value = grovepi.analogRead(SLIDE)
+            ultrasonic_value = grovepi.ultrasonicRead(PORT)
 
-        print(grovepi.ultrasonicRead(PORT))
-'''
-from grove_rgb_lcd import *
-import grovepi
-import time
 
-time.sleep(2)
-slide = 0
+            if (previous_sensor_value != sensor_value and abs(previous_sensor_value - sensor_value) > 2):
+                print("sensor_value =", sensor_value)
+                if(sensor_value >= ultrasonic_value):
+                    setText(str(sensor_value)+"cm OBJ PRES \n" + str(ultrasonic_value) + "cm" )
+                else:
+                    setText(str(sensor_value)+"cm\n" + str(ultrasonic_value) + "cm" )
+            previous_sensor_value = sensor_value
+        except IOError:
+            print("Error")
+        
 
-led = 1
-
-grovepi.pinMode(slide,"INPUT")
-grovepi.pinMode(led,"OUTPUT")
-time.sleep(1)
-setRGB(0,255,0)
-setText(" ")
-previous_sensor_value = -1
-while True:
-	time.sleep(0.6)
-	try:
-		sensor_value = grovepi.analogRead(slide)
-		if sensor_value > 500:
-			grovepi.digitalWrite(led,1)
-		else:
-			grovepi.digitalWrite(led,0)
-		if (previous_sensor_value != sensor_value and abs(previous_sensor_value - sensor_value) > 2):
-			print("sensor_value =", sensor_value)
-			setText(str(sensor_value)+"cm")
-		previous_sensor_value = sensor_value
-	except IOError:
-		print("Error")
