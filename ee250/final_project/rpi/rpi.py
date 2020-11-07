69b1e81066921686563135c6b30e42819d1d42cb
Daniel D'Souza <dmdsouza@usc.edu>
2020-11-07 14:13:13 -0800

    fix#2

--- ee250/final_project/rpi/rpi.py
+++ ee250/final_project/rpi/rpi.py
@@ -4,33 +4,24 @@ import time
 
 sys.path.append('/home/pi/Dexter/GrovePi/Software/Python')
 
-import grovepi
-import grove_rgb_lcd as lcd
+
 
 import math
 
-# Connect the Grove Temperature & Humidity Sensor Pro to digital port D4
-# This example uses the blue colored sensor.
-# SIG,NC,VCC,GND
-sensor = 4  # The Sensor goes on digital port 4.
+from grovepi import *
+from grove_oled import *
 
-# temp_humidity_sensor_type
-# Grove Base Kit comes with the blue sensor.
-blue = 0    # The Blue colored sensor.
-white = 1   # The White colored sensor.
+dht_sensor_port = 7     # Connect the DHt sensor to port 7
+
+time.sleep(.1)
 
 while True:
     try:
-        # This example uses the blue colored sensor. 
-        # The first parameter is the port, the second parameter is the type of sensor.
-        print("stuck at dht")
-        [temp,humidity] = grovepi.dht(sensor,blue)  
-        print("still stuck at dht")
-        if math.isnan(temp) == False and math.isnan(humidity) == False:
-            print("temp = %.02f C humidity =%.02f%%"%(temp, humidity))
-        else:
-            print("nan values")
+        dht(dht_sensor_port,0)
         time.sleep(0.5)
+        [ temp,hum ] = dht(dht_sensor_port,1)       #Get the temperature and Humidity from the DHT sensor
 
-    except IOError:
-        print ("Error")
+        print("temp =", temp, "C\thumidity =", hum,"%") 
+        time.sleep(1)   
+    except (IOError,TypeError) as e:
+        print("Error")
