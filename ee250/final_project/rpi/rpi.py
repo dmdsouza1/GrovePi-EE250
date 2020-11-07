ebd6f8b27f58f0bd4ac558ac45004917eb04759e
Daniel D'Souza <dmdsouza@usc.edu>
2020-11-07 14:33:28 -0800

    fix#6

--- ee250/final_project/rpi/rpi.py
+++ ee250/final_project/rpi/rpi.py
@@ -11,22 +11,16 @@ import math
 from grovepi import *
 from grove_oled import *
 
-dht_sensor_port = 7     # Connect the DHt sensor to port 7
+dht_sensor_port = 4     # Connect the DHt sensor to port 7
 
-time.sleep(1)
-print("hello")
+time.sleep(.1)
+
 while True:
     try:
-        time.sleep(1)
-        print("henlo")
-        # dht(dht_sensor_port,0)
-        time.sleep(1)
-        [ temp,hum ] = dht(dht_sensor_port,1)       #Get the temperature and Humidity from the DHT sensor
-        if math.isnan(temp) or  math.isnan(humidity):
-            [temp,humidity] = dht(dht_sensor_port,0)
-            print('temperature ',temp, 'humidity ',humidity)
+        dht(dht_sensor_port,0)
+        time.sleep(0.5)
+        [temp,hum] = dht(dht_sensor_port,1)       #Get the temperature and Humidity from the DHT sensor
 
-        time.sleep(.5)
         print("temp =", temp, "C\thumidity =", hum,"%") 
         time.sleep(1)   
     except (IOError,TypeError) as e:
