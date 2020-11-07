cda80929b5fc7c7cd5bd37b657c35aa55101172d
Daniel D'Souza <dmdsouza@usc.edu>
2020-11-07 14:17:11 -0800

    fix#3

--- ee250/final_project/rpi/rpi.py
+++ ee250/final_project/rpi/rpi.py
@@ -13,12 +13,14 @@ from grove_oled import *
 
 dht_sensor_port = 7     # Connect the DHt sensor to port 7
 
-time.sleep(.1)
-
+time.sleep(1)
+print("hello")
 while True:
     try:
+        time.sleep(1)
+        print("henlo")
         dht(dht_sensor_port,0)
-        time.sleep(0.5)
+        time.sleep(1)
         [ temp,hum ] = dht(dht_sensor_port,1)       #Get the temperature and Humidity from the DHT sensor
 
         print("temp =", temp, "C\thumidity =", hum,"%") 
