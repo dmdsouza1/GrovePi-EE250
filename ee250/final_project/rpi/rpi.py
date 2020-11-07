88eca6348714cc50d0ac181715170dbd359fbb74
Daniel D'Souza <dmdsouza@usc.edu>
2020-11-07 14:06:06 -0800

    trying code to get the temperature sensor to work

--- ee250/final_project/rpi/rpi.py
+++ ee250/final_project/rpi/rpi.py
@@ -7,74 +7,28 @@ sys.path.append('/home/pi/Dexter/GrovePi/Software/Python')
 import grovepi
 import grove_rgb_lcd as lcd
 
-# Modules for my apps
-import my_reddit
-import my_weather
-#import my_app  # TODO: Create my_app.py using another API, following the examples as a template
+import math
 
-PORT_BUZZER = 2     # D2
-PORT_BUTTON = 4     # D4
+# Connect the Grove Temperature & Humidity Sensor Pro to digital port D4
+# This example uses the blue colored sensor.
+# SIG,NC,VCC,GND
+sensor = 4  # The Sensor goes on digital port 4.
 
-LCD_LINE_LEN = 16
-
-# Setup
-grovepi.pinMode(PORT_BUZZER, "OUTPUT")
-grovepi.pinMode(PORT_BUTTON, "INPUT")
-
-lcd.setRGB(0, 128, 0)
-
-# Installed Apps!
-APPS = [
-    my_weather.WEATHER_APP,
-    my_reddit.QOTD_APP,
-    # TODO: Add your new app here
-]
-
-# Cache to store values so we save time and don't abuse the APIs
-CACHE = [''] * len(APPS)
-for i in range(len(APPS)):
-    # Includes a two space offset so that the scrolling works better
-    CACHE[i] = '  ' + APPS[i]['init']()
-
-app = 0     # Active app
-ind = 0     # Output index
+# temp_humidity_sensor_type
+# Grove Base Kit comes with the blue sensor.
+blue = 0    # The Blue colored sensor.
+white = 1   # The White colored sensor.
 
 while True:
     try:
-        # Check for input
-        if grovepi.digitalRead(PORT_BUTTON):
-            # BEEP!
-            grovepi.digitalWrite(PORT_BUZZER, 1)
-
-            # Switch app
-            app = (app + 1) % len(APPS)
-            ind = 0
-
-        time.sleep(0.1)
-
-        grovepi.digitalWrite(PORT_BUZZER, 0)
-
-        # Display app name
-        lcd.setText_norefresh(APPS[app]['name'])
-
-        # Scroll output
-        lcd.setText_norefresh('\n' + CACHE[app][ind:ind+LCD_LINE_LEN])
-        # TODO: Make the output scroll across the screen (should take 1-2 lines of code)
-
-    except KeyboardInterrupt:
-        # Gracefully shutdown on Ctrl-C
-        lcd.setText('')
-        lcd.setRGB(0, 0, 0)
-
-        # Turn buzzer off just in case
-        grovepi.digitalWrite(PORT_BUZZER, 0)
-
-        break
-
-    except IOError as ioe:
-        if str(ioe) == '121':
-            # Retry after LCD error
-            time.sleep(0.25)
-
+        # This example uses the blue colored sensor. 
+        # The first parameter is the port, the second parameter is the type of sensor.
+        [temp,humidity] = grovepi.dht(sensor,blue)  
+        if math.isnan(temp) == False and math.isnan(humidity) == False:
+            print("temp = %.02f C humidity =%.02f%%"%(temp, humidity))
         else:
-            raise
+            print("nan values")
+        time.sleep(0.5)
+
+    except IOError:
+        print ("Error")
