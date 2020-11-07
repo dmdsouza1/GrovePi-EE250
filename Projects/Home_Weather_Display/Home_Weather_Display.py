1c390326cb55095e9053db199e9886c4bb802bb8
Daniel D'Souza <dmdsouza@usc.edu>
2020-11-07 14:02:39 -0800

    changed port from D7 to D4

--- Projects/Home_Weather_Display/Home_Weather_Display.py
+++ Projects/Home_Weather_Display/Home_Weather_Display.py
@@ -42,7 +42,7 @@ from grove_rgb_lcd import *
 from time import sleep
 from math import isnan
 
-dht_sensor_port = 7 # connect the DHt sensor to port 7
+dht_sensor_port = 4 # connect the DHt sensor to port 4
 dht_sensor_type = 0 # use 0 for the blue-colored sensor and 1 for the white-colored sensor
 
 # set green as backlight color
@@ -50,13 +50,14 @@ dht_sensor_type = 0 # use 0 for the blue-colored sensor and 1 for the white-colo
 # setting the backlight color once reduces the amount of data transfer over the I2C line
 setRGB(0,255,0)
 print("started")
-setText_norefresh("hello")
+setText_norefresh("henlo")
 
 while True:
 	try:
 		print("entered try")
         # get the temperature and Humidity from the DHT sensor        
-		[ temp,hum ] = dht(dht_sensor_port,dht_sensor_type)
+		[temp,hum] = dht(dht_sensor_port, dht_sensor_type)
+		print("completed")
 		print("temp =", temp, "C\thumidity =", hum,"%")
 
 		# check if we have nans
